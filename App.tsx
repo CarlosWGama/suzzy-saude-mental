9536e0a262679527e1107ab718155e2285d94ad5
CarlosWGama <carloswgama@gmail.com>
2024-02-15 18:22:17 -0300

    Atualizado SDK 50

--- App.tsx
+++ App.tsx
@@ -3,9 +3,16 @@ import { useFonts, Jura_400Regular, Jura_300Light, Jura_700Bold } from '@expo-go
 import NavegacaoPrincipal from './src/navigation';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ContextAppProvider } from './src/provider/context';
+import { LogBox } from 'react-native';
+
 
 export default function App() {
 
+  // =========== IGNORA AJUSTE SDK 50 ========================
+  LogBox.ignoreLogs(['new NativeEventEmitter']);
+  LogBox.ignoreAllLogs(); 
+  // ===========================================================
+  
   let [fontsLoaded] = useFonts({
     Jura_400Regular, Jura_300Light, Jura_700Bold
 
